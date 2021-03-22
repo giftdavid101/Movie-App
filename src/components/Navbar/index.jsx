d9de84d3a5638cc8f1d35cb39261e18b1d0e3e0b
giftdavid101 <giftdavid101@gmail.com>
2021-03-22 15:50:48 +0100

    created logo component and imported in navbar finally styled navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './navbar.style.css'
+import Logo from "../../elements/Logo";
 
 const NavBar = () => {
     return (
         <div className={'navbar'}>
             <div className={'navbar_nb'}>
-
+                 <Logo/>
                 <div className={'navbar_nb_nav-lnks'}>
-                    <Link to={'/'}>Movies</Link>
-                    <Link to={'/watched'}>Watched</Link>
-                    <Link to={'/watchlist'}>WatchList</Link>
+                    <Link className={'n-l'} to={'/'}>Movies</Link>
+                    <Link className={'n-l'} to={'/watched'}>Watched</Link>
+                    <Link className={'n-l'} to={'/watchlist'}>WatchList</Link>
                 </div>
 
             </div>
