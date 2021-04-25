12723e4670dc084d06b0aeb72c4394abfea4bce9
giftdavid101 <giftdavid101@gmail.com>
2021-04-25 09:46:36 +0100

    my code

--- src/components/compounds/Toprated/index.jsx
+++ src/components/compounds/Toprated/index.jsx
@@ -1,11 +1,28 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import TopRatedCard from "../../elements/TopratedCard";
 import mvRequest from "../../../utils/mvRequest";
-import {GlobalContext} from "../../../context/globalstate";
+
+
+const reshapeTopRatedMovies = (data) =>{
+    return data.reduce((res, cur)=>{
+        res.push({...cur, fav: false})
+        return res;
+    },[])
+}
 
 
 const TopRated = () => {
     const [topRated,setTopRated] = useState([])
+    
+  const handleAddFavourites = (id) => {
+    const ans = topRated.reduce((res,cur,i) => {
+      if(cur.id === id) res.push({ ...cur, fav: !cur.fav });
+      else res.push(cur)
+      return res;
+    }, []);
+    setTopRated(ans);
+
+  };
 
     // const mainUrl = process.env.REACT_APP_API_BASE_LINK
 
@@ -19,9 +36,11 @@ const TopRated = () => {
         mvRequest.get(link).then((response) =>{
             const {data,status} = response
             console.log(response)
+
             if(status === 200){
-                setTopRated(data.results)
+                setTopRated(reshapeTopRatedMovies(data.results));
             }
+          console.log(reshapeTopRatedMovies(data.results));
         }).catch((err) => {
             console.log(err)
         });
@@ -34,20 +53,21 @@ const TopRated = () => {
     },[])
 
     return (
-        <div className={'top-rated'}>
-           <h2 className={'tiny-elements-padding'}>Top Rated Movies</h2>
-            <div className={'top-rated_tp-con grid-style'}>
-                {
-                    topRated.map((el) => (
-                        <TopRatedCard
-                            movie={el}
-                            key={el.id}
-                            emage={el.poster_path}
-                        />
-                    )).slice(-5)
-                }
-            </div>
+      <div className={"top-rated"}>
+        <h2 className={"tiny-elements-padding"}>Top Rated Movies</h2>
+        <div className={"top-rated_tp-con grid-style"}>
+          {topRated
+            .map((el) => (
+              <TopRatedCard
+                handleAddFavourites={handleAddFavourites}
+                movie={el}
+                key={el.id}
+                emage={el.poster_path}
+              />
+            ))
+            .slice(-5)}
         </div>
+      </div>
     );
 };
 
